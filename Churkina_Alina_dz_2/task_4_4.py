2145821b1c42edd635e81a7cbffac268f3714461
Alina Ch <89185720843@bk.ru>
2022-02-08 17:08:08 +0300

    added hw_4

---
+++ Churkina_Alina_dz_2/task_4_4.py
@@ -0,0 +1,11 @@
+import utils
+
+print(utils.currency_rates('GBP'))
+print(utils.currency_rates('AMD'))
+print(utils.currency_rates('HUF'))
+
+"""" Результат из консоли
+101.8791
+0.15599
+0.243207
+"""
