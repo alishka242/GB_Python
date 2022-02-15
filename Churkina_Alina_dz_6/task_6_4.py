b6a1e596731a5cead39a98be5f8d869414921ce1
Alina Ch <89185720843@bk.ru>
2022-02-15 23:26:05 +0300

    added task_6_3 and task_6_4

--- Churkina_Alina_dz_6/task_6_4.py
+++ Churkina_Alina_dz_6/task_6_4.py
@@ -8,20 +8,21 @@ def prepare_dataset(path_users_file: str, path_hobby_file: str) -> dict:
     :param path_hobby_file: путь до файла, содержащий хобби, разделенные запятой по строке
     :return: Dict(str: Union[List[str]|None])
     """
-    dict_hob = {}
+    list_hob = []
     us_name = [x.strip() for x in open(path_users_file, 'r', encoding='utf-8').readlines()]
-    us_hob = [x.strip().split(',') for x in open(path_hobby_file, 'r', encoding='utf-8').readlines()]
+    us_hob = [x.strip() for x in open(path_hobby_file, 'r', encoding='utf-8').readlines()]
 
     if len(us_name) < len(us_hob):
         return 1
     elif len(us_name) > len(us_hob):
         us_hob.append(None)
     for key, val in zip(us_name, us_hob):
-        dict_hob.setdefault(key, val)
-
-    return  dict_hob
+        list_hob.append(f'{key}: {val}\n')
+    print(*list_hob)
+    return  list_hob
 
 dict_out = prepare_dataset('users.csv', 'hobby.csv')
 
-with open('task_6_3_result.json', 'w', encoding='utf-8') as fw:
-    json.dump(dict_out, fw, ensure_ascii=False, indent=2)
+with open('users_hobby.txt', 'w', encoding='utf-8') as fw:
+    for str_h in dict_out:
+        fw.write(str_h)
