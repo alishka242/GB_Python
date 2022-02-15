be3f4a46216ec7ba6f67ffeadc030527857ea8f5
Alina Ch <89185720843@bk.ru>
2022-02-16 00:02:19 +0300

    added task_6_5

---
+++ Churkina_Alina_dz_6/task_6_5.py
@@ -0,0 +1,4 @@
+import utils
+from sys import argv
+
+print(utils.write_in(argv[1], argv[2], argv[3]))
--- Churkina_Alina_dz_6/utils.py
+++ Churkina_Alina_dz_6/utils.py
@@ -13,7 +13,6 @@ def prepare_dataset(path_users_file: str, path_hobby_file: str) -> dict:
     with open(path_hobby_file, 'r', encoding='utf-8') as us_hob:
         us_hob = [x.strip() for x in us_hob.readlines()]
 
-
     if len(us_name) < len(us_hob):
         return "None"
     elif len(us_name) > len(us_hob):
@@ -23,8 +22,11 @@ def prepare_dataset(path_users_file: str, path_hobby_file: str) -> dict:
     print(*list_hob)
     return  list_hob
 
-dict_out = prepare_dataset('users.csv', 'hobby.csv')
 
-with open('users_hobby.txt', 'w', encoding='utf-8') as fw:
-    for str_h in dict_out:
-        fw.write(str_h)
+def write_in(file_from_1:str, file_from_2:str, file_wr:str):
+
+    dict_out = prepare_dataset(file_from_1, file_from_2)
+
+    with open(file_wr, 'w', encoding='utf-8') as fw:
+        for str_h in dict_out:
+            fw.write(str_h)
