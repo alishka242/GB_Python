fa8bc0437b5f619d0c16dc0e26ddf057b8e68bbf
Alina Ch <89185720843@bk.ru>
2022-03-07 15:23:20 +0300

    save task_8_2_

--- Churkina_Alina_dz_8/task_8_2.py
+++ Churkina_Alina_dz_8/task_8_2.py
@@ -1,10 +1,23 @@
 import re
 from pprint import pprint
 
-ip_add = re.compile(r'[0-9]*\.{4}')
+def get_parse_attrs(line: str) -> tuple:
+    """Парсит строку на атрибуты и возвращает кортеж атрибутов (<remote_addr>, <request_type>, <requested_resource>)"""
+    ip_add = re.compile(r'(?:\d*\.){3}\d*') #'188.138.60.101'
+    time_is = re.compile(r'\d*\/\w*\/(?:\d*\:){3}\d+\s\+\d*') #'17/May/2015:08:05:49 +0000'
+    API_req = re.compile(r'GET|POST') #'GET'
+    uri = re.compile(r'\/\w*\/[a-z]*\_\d') #'/downloads/product_2'
+    resp_str = re.compile(r' \d* \d*') #'304', '0'
+
+    list_str = ip_add.findall(line)[0]
+    
+    pprint(list_str)
+
+list_out = list()
 
 with open('nginx_logs.txt', 'r', encoding='utf-8') as fr:
     for str_fr in fr:
         # parsed_raw = ('188.138.60.101', '17/May/2015:08:05:49 +0000', 'GET', '/downloads/product_2', '304', '0')
-        # print(ip_add.findall(str_fr))
-        pprint(str_fr)
+        list_out.append(get_parse_attrs(fr.readline()))
+
+#print(list_out)
