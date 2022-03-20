f797fad0cf6f26b31edbec5cda3f6ddffb7857f4
Alina Ch <89185720843@bk.ru>
2022-03-20 19:33:20 +0300

    task_10_1 has one bug

--- Churkina_Alina_dz_10/task_10_1.py
+++ Churkina_Alina_dz_10/task_10_1.py
@@ -3,13 +3,23 @@ from typing import List
 
 class Matrix:
     def __init__(self, matrix: List[List[int]]):
-        self.matrix = matrix
-        for list_numb in self.matrix:
-            if list(list_numb):
-                for val in list_numb:
-                    if isinstance():
-                        
+        for list_numb in matrix:
+            if (len(matrix) == 3 and len(list_numb) == 2) or (len(matrix) == 2 and len(list_numb) == 4) or (len(matrix) == 3 and len(list_numb) == 3):
+                self.matrix = matrix   
+            else:
+                raise ValueError ('fail initialization matrix')
+                            
+    def __str__(self) -> str:
+        self.martix_str = ""
+        for row in self.matrix:
+            row_str= [str(r_str) for r_str in row]
+            self.martix_str += f'| {" ".join(row_str)} |\n'
 
+        return self.martix_str 
+
+    def __add__(self, other):
+        new_list =  [[self.matrix[i][j] + other.matrix[i][j] for j in range(len(self.matrix[0]))] for i in range(len(self.matrix))]
+        return Matrix(new_list)
 
 if __name__ == '__main__':
     first_matrix = Matrix([[1, 2], [3, 4], [5, 6]])
@@ -27,6 +37,7 @@ if __name__ == '__main__':
     | 7 7 |
     """
     fail_matrix = Matrix([[1, 2], [3, 4, 7], [5, 6]])
+    print(fail_matrix)
     """
     Traceback (most recent call last):
       ...
