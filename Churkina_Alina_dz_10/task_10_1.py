a393301e295e04550378a9c57dc8afdac94339b7
Alina Ch <89185720843@bk.ru>
2022-03-20 17:05:56 +0300

    start

---
+++ Churkina_Alina_dz_10/task_10_1.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+class Matrix:
+    def __init__(self, matrix: List[List[int]]):
+        self.matrix = matrix
+        for list_numb in self.matrix:
+            if list(list_numb):
+                for val in list_numb:
+                    if isinstance():
+                        
+
+
+if __name__ == '__main__':
+    first_matrix = Matrix([[1, 2], [3, 4], [5, 6]])
+    second_matrix = Matrix([[6, 5], [4, 3], [2, 1]])
+    print(first_matrix)
+    """
+    | 1 2 |
+    | 3 4 |
+    | 5 6 |
+    """
+    print(first_matrix + second_matrix)
+    """
+    | 7 7 |
+    | 7 7 |
+    | 7 7 |
+    """
+    fail_matrix = Matrix([[1, 2], [3, 4, 7], [5, 6]])
+    """
+    Traceback (most recent call last):
+      ...
+    ValueError: fail initialization matrix
+    """
