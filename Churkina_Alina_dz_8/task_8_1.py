dea9d87cd68b0d064b14810608370a9b7a14558b
Alina Ch <89185720843@bk.ru>
2022-03-20 17:46:09 +0300

    the taks_8_1 finished

--- Churkina_Alina_dz_8/task_8_1.py
+++ Churkina_Alina_dz_8/task_8_1.py
@@ -6,13 +6,13 @@ def email_parse(email: str) -> dict:
     :param email: строковое входное значение обрабатываемого email
     :return: {'username': <значение до символа @>, 'domain': <значение за символом @>} | ValueError
     """
-    RE_MAIL = re.compile(r'(^\w*)[\@](\w*[\.]+\w{2}$|\w*[\.]+\w{3}$)')
-    dict_emails = {'username': RE_MAIL.findall(email)[0][0], 'domain': RE_MAIL.findall(email)[0][1]}
-    
-    if dict_emails:
-        return dict_emails
+    RE_MAIL = re.compile(r'(^\w*)[\@](\w*[\.]+\w{2}$|\w*[\.]+\w{2,3}$)')
+    VALID_EMAIL = re.compile(r'^\w{1,}\@(\w*[\.]+\w{2}$|\w*[\.]+\w{3}$)')
+
+    if VALID_EMAIL.search(email) is not None:
+        return {'username': RE_MAIL.findall(email)[0][0], 'domain': RE_MAIL.findall(email)[0][1]}
     else:
-        raise ValueError (f'wrong email: {email}')
+        raise ValueError(f'wrong email: {email}')
 
 if __name__ == '__main__':
     email_parse('someone@geekbrains.ru')
